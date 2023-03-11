cae5070bd69e51cca4ab14ff131818d4206373f9
Eztas <91040641+Eztas@users.noreply.github.com>
2023-03-11 18:53:56 +0900

    Update open_result_screen.js

--- javascript/open_result_screen.js
+++ javascript/open_result_screen.js
@@ -3,5 +3,6 @@ function open_other_window(score_to_kill_sat) {
     var username = new URLSearchParams(window.location.search).get("username");
     sessionStorage.setItem('limit_break', limit_break_id);
     //localStorage.setItem('limit_break', limit_break_id);
-    window.location.href = "result.html?value="+score_to_kill_sat+"&username="+username;
+    //window.location.href = "result.html?value="+score_to_kill_sat+"&username="+username; // safariでやると, 戻った時に前の記録が残ったままだった
+    window.location.replace("result.html?value="+score_to_kill_sat+"&username="+username);
 }
