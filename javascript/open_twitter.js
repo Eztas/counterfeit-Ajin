9e3bf5d94a1d4f2be31974445f735d731281996b
Eztas <91040641+Eztas@users.noreply.github.com>
2023-03-11 10:36:15 +0900

    Update open_twitter.js

--- javascript/open_twitter.js
+++ javascript/open_twitter.js
@@ -1,10 +1,8 @@
 // Twitterの画面を開いて, 自動遷移
-//open_report_Twitter("I killed " + String(kill_sat_num) + "." ,"https://eztas.github.io/","Ajin","anime_ajin");
 function open_other_window(score_to_kill_sat) {
     const text = "I killed " + String(score_to_kill_sat) + ".";
-    const url = "https://eztas.github.io/";
+    const url = "https://afternoon.kodansha.co.jp/c/ajin.html";
     const hash = "Ajin";
     const account = "anime_ajin";
     window.location.href = "https://twitter.com/intent/tweet?text="+text+"&url="+url+"&hashtags="+hash+"&via="+account;
-    //document.write(text); // Twitterに飛ぶのがうざいとき
 }
